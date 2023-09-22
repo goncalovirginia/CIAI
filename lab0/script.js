3ce5c5a2937626d712024e04096e9b1ccc8b56d2
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-09-22 19:11:11 +0100

    route select elements initialization implemented

--- lab0/script.js
+++ lab0/script.js
@@ -1,14 +1,63 @@
 
+const terminalCodeToName = {
+	"TSA": "Tsawwassen",
+	"SWB": "Swartz Bay",
+	"SGI": "Southern Gulf Islands",
+	"DUK": "Duke Point",
+	"FUL": "Fulford Harbour",
+	"HSB": "Horseshoe Bay",
+	"NAN": "Departure Bay (Nanaimo)",
+	"LNG": "Langford",
+	"BOW": "Bowen Island"
+};
+
+const terminalDestinations = {
+	"TSA": ["SWB", "SGI", "DUK"],
+	"SWB": ["TSA", "FUL", "SGI"],
+	"HSB": ["NAN", "LNG", "BOW"],
+	"DUK": ["TSA"],
+	"LNG": ["HSB"],
+	"NAN": ["HSB"],
+	"FUL": ["SWB"],
+	"BOW": ["HSB"]
+}
+
+const selectFrom = document.getElementById("selectFrom");
+const selectTo = document.getElementById("selectTo");
+
+selectFrom.addEventListener("select", updateListing);
+selectTo.addEventListener("select", updateListing);
+
+initializeSelectElements();
+
+function initializeSelectElements() {
+	Object.keys(terminalDestinations).forEach(terminalCode => {
+		appendSelectOption(selectFrom, terminalCode, terminalCodeToName[terminalCode]);
+	});
+	terminalDestinations["TSA"].forEach(terminalCode => {
+		appendSelectOption(selectTo, terminalCode, terminalCodeToName[terminalCode]);
+	});
+}
+
+function appendSelectOption(selectElement, value, text) {
+	const option = document.createElement("option");
+	option.setAttribute("value", value);
+	option.innerHTML = text;
+	selectElement.appendChild(option);
+}
+
 async function fetchFerries(from, to) {
-	const response = (await fetch("https://www.bcferriesapi.ca/api/" + from + "/" + to));
+	const url = "https://www.bcferriesapi.ca/api/" + from + "/" + to + "/";
+	const response = await fetch(url, {
+		mode: 'cors'
+	});
 	const data = await response.json();
 	console.log(data);
 }
 
-fetchFerries("TSA", "SWB");
-
-
-
-
+function updateListing() {
+	fetchFerries(selectFrom.value, selectTo.value);
+	console.log("updated");
+}
 
 
