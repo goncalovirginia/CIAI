61e2fdb9957ad30d443a477303dd88497c98accd
Gonçalo <gonvirginia.blueslime@gmail.com>
2023-09-22 05:24:46 +0100

    changed API since original API is in maintenance

--- lab0/script.js
+++ lab0/script.js
@@ -1,15 +1,12 @@
 
-const KEY = "p4fA29gHA0VZkJ6kzd8jXXM9CvYa";
-const SECRET = "jwyqobX3GI85pQQZ4N_jLB978Aga"
-const TOKEN = "b07c687b-700a-34aa-935f-821f375d22c4";
-
-async function fetchTest() {
-	const response = await fetch("https://api.metrolisboa.pt:8243/estadoServicoML/1.0.1/tempoEspera/Estacao/todos");
+async function fetchFerries(from, to) {
+	const response = (await fetch("https://www.bcferriesapi.ca/api/" + from + "/" + to));
 	const data = await response.json();
 	console.log(data);
 }
 
-fetchTest();
+fetchFerries("TSA", "SWB");
+
 
 
 
